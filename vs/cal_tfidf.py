b480f5ff20d0f4d0dedb376b124fd66258e041f6
catrossim <catrossim@gmail.com>
2016-09-20 16:01:40 +0800

    organize the code in cal_tfidf.py

--- vs/cal_tfidf.py
+++ vs/cal_tfidf.py
@@ -3,8 +3,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import os
 import logging
-logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
-logging.root.setLevel(level=logging.INFO)
 import codecs
 
 def tokenize(post):
@@ -33,22 +31,16 @@ def swap(arr,i,j):
     arr[i] = arr[j]
     arr[j] = temp
 
-corpus = []
-target = 'vtext'
-s = []
-logging.info('Ready to read sources from %s' % target)
-for path,dir,files in os.walk(target):
-    for file in files:
-        if file.endswith('.text'):
-            with open(os.path.join(path,file),'rU') as f:
-                corpus.append(f.read())
-            s.append(file)
-if not corpus:
-    logging.error('Can not find corpus')
-    import sys
-    sys.exit(1)
-
-logging.info('Source reading complete!')
+def readsrc(target):
+    corpus = []
+    s = []
+    for path,dir,files in os.walk(target):
+        for file in files:
+            if file.endswith('.text'):
+                with open(os.path.join(path,file),'rU') as f:
+                    corpus.append(f.read())
+                s.append(file)
+    return corpus,s
 
 def save_file(path, content):
     logging.info('saving content to %s' % path)
@@ -56,19 +48,32 @@ def save_file(path, content):
         f.write(content)
     logging.info('file %s save' % path)
 
-dest = 'result'
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+    logging.root.setLevel(level=logging.INFO)
 
-v = TfidfVectorizer(tokenizer=tokenize)
-logging.info('Begin to calculate tf-idf...')
-result = v.fit_transform(corpus).toarray()
-logging.info('Successfully get tf-idf vector!')
-words = v.get_feature_names()
+    target = 'vtext'
+    logging.info('Ready to read sources from %s' % target)
+    corpus, s = readsrc(target)
+    if not corpus:
+        logging.error('Can not find corpus')
+        import sys
+        sys.exit(1)
+    logging.info('Source reading complete!')
 
-for r in xrange(len(result)):
-    output = []
-    logging.info('Begin to sort %s...' % r)
-    sorted_index = get_sorted_index(result[r])
-    logging.info('Sorted %s' %r)
-    for i in sorted_index:
-        output.append(words[i]+' '+str(result[r][i]))
-    save_file(os.path.join(dest,s[r]+'.words'), '\n'.join(output))
+    dest = 'result'
+
+    v = TfidfVectorizer(tokenizer=tokenize)
+    logging.info('Begin to calculate tf-idf...')
+    result = v.fit_transform(corpus).toarray()
+    logging.info('Successfully get tf-idf vector!')
+    words = v.get_feature_names()
+
+    for r in xrange(len(result)):
+        output = []
+        logging.info('Begin to sort %s...' % r)
+        sorted_index = get_sorted_index(result[r])
+        logging.info('Sorted %s' %r)
+        for i in sorted_index:
+            output.append(words[i]+' '+str(result[r][i]))
+        save_file(os.path.join(dest,s[r]+'.words'), '\n'.join(output))
