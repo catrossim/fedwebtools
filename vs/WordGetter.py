c05f00fd8a1834e705a10c819b7edd65b91c2774
catrossim <catrossim@gmail.com>
2016-09-27 16:46:27 +0800

    add WordGetter.py for word2vec use

---
+++ vs/WordGetter.py
@@ -0,0 +1,13 @@
+import gensim, logging
+class WordGetter(object):
+
+    def __init__(self, path):
+        logging.info('Loading model from %s' % path)
+        self.model = _load_model(path, binary)
+        logging.info('Model is loaded.')
+
+    def _load_model(path, binary):
+        return gensim.models.Word2Vec.load(path)
+
+    def most_similar(words, topn=10):
+        return self.model.most_similar(positive=words, topn=topn)
