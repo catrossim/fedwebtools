a18ab18c14fb81bf50e568215184d2e5efbe026a
catrossim <catrossim@gmail.com>
2016-10-08 15:56:51 +0800

    fix some bugs

--- vs/query_expand.py
+++ vs/query_expand.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         for line in f.readlines():
             query = line.split('\t')[1]
             words = preprocess_query(query)
-            simwords = get_similar_words(words)]
+            simwords = get_similar_words(words)
             r = zip([a[0] for a in simwords],map(str,[a[1] for a in simwords]))
-            result = '\n'.join([' '.join(x) for x in r)
+            result = '\n'.join([' '.join(x) for x in r])
             save(os.path.join(dest_dir,query[0]), result)
