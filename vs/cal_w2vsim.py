013d040e9c76fc9c12a0b3a3a55e3345a45df1b3
catrossim <catrossim@gmail.com>
2016-10-20 21:13:31 +0800

    add method normalize for w2vsim calculating

--- vs/cal_w2vsim.py
+++ vs/cal_w2vsim.py
@@ -31,8 +31,7 @@ def calW2vSim(wordgetter, words, vmap):
             for key in vdict.iterkeys():
                 try:
                     score = wordgetter.similarity(word,key)
-                except KeyError, arg:
-                    logging.warn(arg)
+                except KeyError:
                     score = 0
                 sum += score
         result[v] = sum/wordslen
@@ -55,9 +54,18 @@ def save(path,content):
     with codecs.open(path,'w','utf-8') as f:
         f.write(content)
 
+def normalize(scores):
+    result = []
+    max = scores[0][1]
+    min = scores[len(scores)-1][1]
+    for s in scores:
+        result.append((s[0],(s[1]-min)*1.0/(max-min)))
+    return result
+
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
     logging.root.setLevel(level=logging.INFO)
+
     model_path = 'enwiki_nltk/wiki.en.text.model'
     q_dir = 'expqs'
     v_dir = 'tfidfs'
@@ -65,16 +73,21 @@ if __name__ == '__main__':
     vlimit = int(sys.argv[1]) if len(sys.argv)>1 else 200
     dest = 'rsim_w2v_'+str(vlimit)
     logging.info('q_dir: %s, v_dir: %s, dest: %s' %(q_dir, v_dir, dest))
+
     if not os.path.exists(dest):
         os.mkdir(dest)
         logging.info('%s created' %dest)
+
     vmap = readvToDict(v_dir, vlimit)
     logging.info('%s verticals were read.' %len(vmap))
+
     q_files = [file for file in os.listdir(q_dir) if file.isdigit()]
     logging.info('%s queries were loaded.' %len(q_files))
+
     for q_file in q_files:
         exp_words = getExpWords(os.path.join(q_dir,q_file))
         result = calW2vSim(wordgetter, exp_words, vmap)
         sorted_r = sorted(result.items(),key=itemgetter(1),reverse=True)
+        sorted_r = normalize(sorted_r)
         content = zip([a[0] for a in sorted_r],map(str, [a[1] for a in sorted_r]))
         save(os.path.join(dest, q_file), '\n'.join([' '.join(x) for x in content]))
