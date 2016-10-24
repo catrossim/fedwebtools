379064e193f807a6cb126453b15d96854e7e32e1
catrossim <catrossim@gmail.com>
2016-10-24 20:10:46 +0800

    add logging info

--- rs/gen_resource_topn.py
+++ rs/gen_resource_topn.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from gensim.models import LdaModel
 from codecs import open as open
 from multiprocessing import Pool
-import os, sys
+import os, sys, logging
+logger = logging.getLogger('gen_lda')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 def load_model(path):
     return LdaModel.load(path)
 
@@ -10,6 +19,7 @@ def save(path, content):
         f.write(content)
 
 def get_topn_words_and_save(path, dest):
+    logger.info('%s begin' %path)
     model_name = os.path.basename(path)+'.model'
     lda_model = load_model(os.path.join(path,model_name))
     wordmap = lda_model.id2word
@@ -20,8 +30,12 @@ def get_topn_words_and_save(path, dest):
     for t in topics:
         r = [(wordmap[x[0]],str(x[1])) for x in lda_model.get_topic_terms(t,30)]
         save(os.path.join(destpath, str(t)), '\n'.join([' '.join(x) for x in r]))
+    logger.info('%s finish' %path)
 
 if __name__ == '__main__':
+    if len(sys.argv)<3:
+        print 'usage: ./gen_resource_topn.py <src dir> <dest>'
+        sys.exit(1)
     src = sys.argv[1]
     dest = sys.argv[2]
     if not os.path.exists(dest):
