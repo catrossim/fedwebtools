9cb5a41bd55e75274a3206a3a494a2fc4dc3d90a
catrossim <catrossim@gmail.com>
2016-10-27 16:23:03 +0800

    update script gen_result

--- vs/gen_result.py
+++ vs/gen_result.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 import codecs,os
 import logging
-GENERAL_SRC = 'FW14-v001'
-QUESTION_WORDS = ['what','when','how','where']
+GENERAL_SRC = 'v001'
+QUESTION_WORDS = ['who','why','what','when','how','where']
 FORMAT_STR = '{} {} {}'
 qterms = {}
 
@@ -12,6 +12,7 @@ def readQTerms():
         for line in f.readlines():
             tokens = line.split('\t')
             qterms[tokens[0]] = tokens[1]
+
 def findQWords(name):
     for w in QUESTION_WORDS:
         if qterms.get(name).find(w)>=0:
@@ -21,23 +22,29 @@ def findQWords(name):
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
     logging.root.setLevel(level=logging.INFO)
-    dest = 'kapokrun1'
-    source = 'rsim'
-    esp=1e-2
+    if len(sys.argv)<4:
+        print 'usage: python genresult.py <src> <runtag> <esp>'
+    source = sys.argv[1]
+    dest = sys.argv[2]
+    esp = float(sys.argv[3])
     output = []
     readQTerms()
     for fname in os.listdir(source):
         count = 0
         with codecs.open(os.path.join(source,fname),'r','utf-8') as f:
+            flag = False
             if findQWords(fname):
                 logging.info('%s has question-word. Send it to GENERAL' %fname)
-                output.append(FORMAT_STR.format(fname,GENERAL_SRC,dest))
+                output.append(FORMAT_STR.format(fname,'FW14-'+GENERAL_SRC,dest))
                 count += 1
+                flag = True
             for line in f.readlines():
                 tokens = line.split(' ')
                 if tokens[1] == '0':
                     break
                 if float(tokens[1].strip()) > esp:
+                    if flag and tokens[0]==GENERAL_SRC:
+                        continue
                     output.append(FORMAT_STR.format(fname,'FW14-'+tokens[0],dest))
                     count += 1
             if not count:
