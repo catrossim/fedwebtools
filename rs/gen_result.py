dbbff5b4c921a594e61cef2ef6d7f9c52e299a2a
catrossim <catrossim@gmail.com>
2016-10-29 19:30:43 +0800

    update gen_result.py

--- rs/gen_result.py
+++ rs/gen_result.py
@@ -22,52 +22,11 @@ if __name__ == '__main__':
     runtag = sys.argv[2]
     fstr = '{} Q0 {} {} {} {}'
     result = []
-    qterm_path = sys.argv[3]
-    readQTerms(qterm_path)
     for file in os.listdir(src):
-        if not file.isdigit():
-            continue
         rank = 1
-        f1 = False
-        if findQWords(file):
-            result.append(
-                fstr.format(
-                    file,
-                    'FW14-'+GENERAL_RESOURCE,
-                    rank,
-                    380,
-                    runtag
-                )
-            )
-            f1 = True
-            rank += 1
         lines = readfilebylines(os.path.join(src,file))
-        f2 = False
         for line in lines:
             token = line.strip().split(' ')
-            if f1 and token[0] == GENERAL_RESOURCE:
-                continue
-            if not f1 and not f2 and abs(float(token[1])-0.0) <= 1e-2:
-                result.append(
-                    fstr.format(
-                        file,
-                        'FW14-'+GENERAL_RESOURCE,
-                        rank,
-                        380,
-                        runtag
-                    )
-                )
-                f1 = True
-                f2 = True
-                rank += 1
-            result.append(
-                fstr.format(
-                    file,
-                    'FW14-'+token[0],
-                    rank,
-                    token[1],
-                    runtag
-                    )
-                )
+            result.append(fstr.format(file, 'FW14-'+token[0], rank, token[1], runtag))
             rank += 1
     save(runtag, '\n'.join(result))
