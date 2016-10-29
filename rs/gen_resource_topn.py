2559fd449db4b235447d04fa02001a79f9ec3b1c
catrossim <catrossim@gmail.com>
2016-10-29 12:32:15 +0800

    add custom topn in gen_resource_topn

--- rs/gen_resource_topn.py
+++ rs/gen_resource_topn.py
@@ -14,7 +14,7 @@ logger.addHandler(handler)
 def load_model(path):
     return LdaModel.load(path)
 
-def get_topn_words_and_save(path, dest):
+def get_topn_words_and_save(path, dest, topn):
     logger.info('%s begin' %path)
     model_name = os.path.basename(path)+'.model'
     lda_model = load_model(os.path.join(path,model_name))
@@ -24,21 +24,22 @@ def get_topn_words_and_save(path, dest):
     if not os.path.exists(destpath):
         os.mkdir(destpath)
     for t in topics:
-        r = [(wordmap[x[0]],str(x[1])) for x in lda_model.get_topic_terms(t,30)]
+        r = [(wordmap[x[0]],str(x[1])) for x in lda_model.get_topic_terms(t,topn)]
         save(os.path.join(destpath, str(t)), '\n'.join([' '.join(x) for x in r]))
     logger.info('%s finish' %path)
 
 if __name__ == '__main__':
     if len(sys.argv)<3:
-        print 'usage: ./gen_resource_topn.py <src dir> <dest>'
+        print 'usage: ./gen_resource_topn.py <src> <dest> <n>'
         sys.exit(1)
     src = sys.argv[1]
     dest = sys.argv[2]
+    n = sys.argv[3]
     if not os.path.exists(dest):
         os.mkdir(dest)
     p = Pool()
     for d in os.listdir(src):
         if d.startswith('e'):
-            p.apply_async(get_topn_words_and_save, args=(os.path.join(src,d), dest))
+            p.apply_async(get_topn_words_and_save, args=(os.path.join(src,d), dest, ns))
     p.close()
     p.join()
