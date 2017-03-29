1ba300625349b7914f4026116e72520a2c38aaa4
catrossim <catrossim@gmail.com>
2017-03-29 11:05:00 +0800

    add script for calculating similarity by KL-Divergence

---
+++ rs/rs_ldx.py
@@ -0,0 +1,60 @@
+#!/bin/bash
+from rs_utils import readfilebylines
+import os,sys
+from scipy.stats import entropy
+def cal_sim(qmat, rmat):
+    return entropy(qmat, rmat)
+
+import pandas as pd
+if __name__ == '__main__':
+    if len(sys.argv)<7:
+        print 'usage: python %s <qorigin> <qmat_dir> <lda_dir> <rmat_dir> <dest> <runID>'%sys.argv[0]
+        sys.exit()
+    qsequence = {}
+    count = 0
+    qorigin = sys.argv[1]
+    qmat_dir = sys.argv[2]
+    lda_dir = sys.argv[3]
+    rmat_dir = sys.argv[4]
+    dest = sys.argv[5]
+    runID = sys.argv[6]
+    for qname in os.listdir(qorigin):
+        qsequence[count] = qname
+        count += 1
+    qmats = {}
+    for e in os.listdir(qmat_dir):
+        s = pd.read_csv(
+            os.path.join(
+                lda_dir,
+                e,
+                'expqs_google_inf.model-final.theta'
+            ),
+            header=None,sep=' '
+        )
+        del s[10]
+        qmats[e] = s
+    rmats = {}
+    for r in os.listdir(rmat_dir):
+        rmat = np.load(os.path.join(rmat_dir,r,r+'.npy'))
+        print r
+        rmats[r] = rmat
+    result = {}
+    for k,v in qsequence.iteritems():
+        qresult = {}
+        for qk,qv in qmats.iteritems():
+            qmat = qv.iloc[k].tolist()
+            rmat = rmats[qk][0].tolist()
+            score = cal_sim(qmat,rmat)
+            qresult[qk] = score
+        result[k] = qresult
+#     print result
+    out_seq = '%s Q0 FW14-%s %s %s %s'
+    out_result = []
+    for k,v in result.iteritems():
+        q = qsequence[k]
+        sorted_e = sorted(v.items(),key=lambda d:d[1],reverse=True)
+        rank = 1
+        for ek,ev in sorted_e:
+            out_result.append(out_seq%(q, ek, rank, ev, runID))
+            rank += 1
+    save(os.path.join('resource',runID),'\n'.join(out_result))
